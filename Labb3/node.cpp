e2a3a98496496dea1023815b6373d2054bb61129
chren574 <chren564@student.liu.se>
2015-05-11 18:40:23 +0200

    started on insert, lab 3

--- Labb3/node.cpp
+++ Labb3/node.cpp
@@ -12,7 +12,7 @@
 
 
 Node::Node(ELEMENT v, Node *l, Node *r)
- : value(v), left(l), right(r)
+    : value(v), left(l), right(r)
 {
     l_thread = r_thread = false;
 }
@@ -31,11 +31,72 @@ Node::~Node()
 //Otherwise, return false --v already exists in the tree
 bool Node::insert(ELEMENT v)
 {
-    //ADD CODE
-    return false;
+    if(!l_thread || !r_thread)
+    {
+
+        if(stoi(v.first) < stoi(value.first) )
+        {
+            left->insert(v);
+
+        }
+        else if(stoi(v.first) > stoi(value.first))
+        {
+            right->insert(v);
+
+        }
+        else
+        {
+            // found same key -> do not insert
+            value->second++;
+            return false;
+        }
+    }
+
+    if(stoi(v.first) < stoi(value.first)){
+        // Pekar på fel sätt
+
+        if(left == root){
+            left = new Node(v, root, this);
+            left->l_thread = true;
+            l_thread = false;
+        }
+        if(right == root){
+            right = new Node(v, this, root);
+            right->r_thread = true;
+            r_thread = false;
+        }
+
+        left = new Node(v, this, this);
+    }else{
+        // Pekar på fel sätt
+        right = new Node(v, this, this);
+    }
+
+    return true;
+*/
+/*
+    // iterative version
+    Node* temp = this;
+
+    while(!temp->l_thread || !temp->r_thread)
+    {
+        if(stoi(v.first) < stoi(temp->value.first) )
+        {
+            temp = temp->left;
+        }
+        else if(stoi(v.first) > stoi(temp->value.first))
+        {
+            temp = temp->right;
+        }
+        else
+        {
+            // found same key -> do not insert
+            return false;
+        }
+    }
+    */
+
 }
-
-
 
 //Remove the key from the tree having as root this node
 //Return true, if a node storing key was found and deleted
@@ -62,7 +123,7 @@ bool Node::remove(string key, Node* parent, bool isRight)
 //2c: a right child with no children
 void Node::removeMe(Node* parent, bool isRight)
 {
-   //ADD CODE
+    //ADD CODE
 }
 
 
